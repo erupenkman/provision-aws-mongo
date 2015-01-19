48b15f6507702a7de146b54786306b5190c86c07
eru <epenkman@thoughtworks.com>
2015-01-19 21:34:51 +1000

    follow diferent tutorial

--- provision_mongo/Recipe/default.rb
+++ provision_mongo/Recipe/default.rb
@@ -1,6 +1,3 @@
 chef::Log.info("starting")
+include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb::default"
-chef::Log.info("Installing mogno time")
-mongodb_instance "mongodb" do
-  port node['application']['port']
-end
